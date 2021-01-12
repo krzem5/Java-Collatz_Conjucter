eb77f39bac1f6589b751113d14d42deedc81694e
Krzem <krzem5.dev@gmail.com>
2021-01-12 19:59:52 +0100

    Push Update 01/12/2021, 19:51:23

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/collatz_conjucter/Main.java&&java com/krzem/collatz_conjucter/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/collatz_conjucter/Main.java&&jar cvmf ../manifest.mf ../build/collatz_conjucter.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="collatz_conjucter.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/collatz_conjucter.jar
+:end
