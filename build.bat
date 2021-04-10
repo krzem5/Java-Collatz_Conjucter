d00c56e716d8ead348d3e9d9c2c5fd88794c1b99
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:23:04 +0200

    Push Update 04/10/2021, 09:22:46

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/collatz_conjucter/Main.java&&jar cvmf ../manifest.mf ../build/collatz_conjucter.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="collatz_conjucter.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/collatz_conjucter.jar
-:end
+python build.py %*
